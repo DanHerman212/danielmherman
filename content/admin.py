776b6799ec2ec68b06c4c8e7a3f3fa003b187b97
Dan Herman <dan.herman@me.com>
2026-01-02 16:38:14 -0500

    udpated admin.py

--- content/admin.py
+++ content/admin.py
@@ -4,3 +4,25 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Category, Article, Service, ContactMessage
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'title', 'order', 'is_active']
+    list_editable = ['order', 'is_active']
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'published_date', 'is_published']
+    list_filter = ['category', 'is_published', 'published_date']
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['title', 'order', 'is_active']
+    list_editable = ['order', 'is_active']
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'subject', 'created_at', 'is_read']
+    list_filter = ['is_read', 'created_at']
+    readonly_fields = ['name', 'email', 'subject', 'message', 'created_at']
